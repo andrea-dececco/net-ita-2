b21b783010e6b6e5801fddc9f2c4cd61839beaa1
John <aby.vecchio@gmail.com>
2023-11-08 14:14:13 +0100

    prima esercitazione in live

---
+++ prima_live/prima_live/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("inseri due numeri per la somma");
+
+String primoInput = Console.ReadLine();
+String secondoInput = Console.ReadLine();
+
+bool primo = int.TryParse(primoInput, out int result1);
+bool secondo = int.TryParse(secondoInput, out int result2);
+
+int somma = result1 + result2;
+if (primo != true || secondo != true)
+{
+    Console.WriteLine("inserimento non valido");
+
+}
+else
+{
+    Console.WriteLine($"la somma di {result1} + {result2} è {somma}");
+}
