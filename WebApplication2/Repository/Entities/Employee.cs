2a4b3fcfe1eb41dd1ace35fff278e79c30ca6b6c
AndreaFusari <andrea.fusari@hotmail.com>
2023-12-13 10:44:45 +0100

    commit2

--- WebApplication2/Repository/Entities/Employee.cs
+++ WebApplication2/Repository/Entities/Employee.cs
@@ -1,6 +1,14 @@
-﻿namespace WebApplication2.Repository
+﻿namespace WebApplication2.Repository.Entities
 {
     public class Employee
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        public decimal? Ral { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? BirthPlace { get; set; }  
+       
     }
 }
