6c1bcddc06aca5164d7d56b1aa7aac529125208f
MichaelB94 <124793101+MichaelB94@users.noreply.github.com>
2023-12-13 11:06:45 +0100

    Repository added

---
+++ WebApplication2/Repository/EmployeeRepository.cs
@@ -0,0 +1,31 @@
+﻿using WebApplication2.Repository.Entities;
+
+namespace WebApplication2.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public static List<Employee> employees = new List<Employee>();
+
+        public void AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+        public List<Employee> GetAll()
+        {
+            return employees;
+        }
+
+        public Employee? GetById(Guid id)
+        {
+            return employees.Where(e => e.Id == id).FirstOrDefault();
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
--- WebApplication2/Repository/IEmployeeRepository.cs
+++ WebApplication2/Repository/IEmployeeRepository.cs
@@ -5,7 +5,7 @@ namespace WebApplication2.Repository
     public interface IEmployeeRepository
     {
         List<Employee> GetAll();
-        Employee GetById(Guid id);
+        Employee? GetById(Guid id);
         void AddEmployee(Employee employee);
         void UpdateEmployee(Employee employee);
     }
