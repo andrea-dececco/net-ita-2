6f4c8ccc14990756d2b71b51510cb702f81a1466
AndreaFusari <andrea.fusari@hotmail.com>
2023-12-13 10:43:41 +0100

    commit

--- WebApplication2/Repository/IEmployeeRepository.cs
+++ WebApplication2/Repository/IEmployeeRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace WebApplication2.Repository
+﻿using WebApplication2.Repository.Entities;
+
+namespace WebApplication2.Repository
 {
     public interface IEmployeeRepository
     {
+        List<Employee> GetAll();
+        Employee GetById(Guid id);
+        void AddEmployee(Employee employee);
+        void UpdateEmployee(Employee employee);
     }
 }
